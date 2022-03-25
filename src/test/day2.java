b62858dfe022badbf3ad96d8c527ee7866e134ca
Rahul Illa <illarahul2700@gmail.com>
2022-03-25 12:59:53 +0530

    Architect Asian

--- src/test/day2.java
+++ src/test/day2.java
@@ -13,31 +13,25 @@ public class day2 {
 	{
 		System.out.println("Rahul");
 		System.out.println("Rahul");
-		System.out.println("Rahul");
-		System.out.println("Rahul");
+		
 	} 
 	@Test
 	public void loan2()
 	{
 		System.out.println("Murthy");
-		System.out.println("Murthy");
-		System.out.println("Murthy");
-		System.out.println("Murthy");
+		
 	} 
 	public void loan3()
 	{
 		System.out.println("Illa");
 		System.out.println("Illa");
-		System.out.println("Illa");
-		System.out.println("Illa");
+		
 	} 
 	@BeforeTest
 	public void beforetest()
 	{
 		System.out.println("I will execute at  first of test"); 
-		System.out.println("I will execute at  first of test"); 
-		System.out.println("I will execute at  first of test"); 
-		System.out.println("I will execute at  first of test"); 
+		
 		
 	} 
 	@BeforeClass
@@ -45,16 +39,13 @@ public class day2 {
 	{
 		System.out.println("I will execute before methods of class day2"); 
 		System.out.println("I will execute before methods of class day2"); 
-		System.out.println("I will execute before methods of class day2"); 
-		System.out.println("I will execute before methods of class day2"); 
+		
 	} 
 	@AfterClass
 	public void afterclass()
 	{
 		System.out.println("I will execute after methods of class day2"); 
-		System.out.println("I will execute after methods of class day2"); 
-		System.out.println("I will execute after methods of class day2"); 
-		System.out.println("I will execute after methods of class day2"); 
+		
 	} 
 	
 }
