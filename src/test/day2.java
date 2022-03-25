5e589dd59bacb06ae019a6c3d678e6b0274f83b8
Rahul Illa <illarahul2700@gmail.com>
2022-03-25 12:35:29 +0530

    this is 2nd commit

--- src/test/day2.java
+++ src/test/day2.java
@@ -12,29 +12,35 @@ public class day2 {
 	public void loan()
 	{
 		System.out.println("Rahul");
+		System.out.println("Rahul");
 	} 
 	@Test
 	public void loan2()
 	{
 		System.out.println("Murthy");
+		System.out.println("Murthy");
 	} 
 	public void loan3()
 	{
+		System.out.println("Illa");
 		System.out.println("Illa");
 	} 
 	@BeforeTest
 	public void beforetest()
 	{
 		System.out.println("I will execute at  first of test"); 
+		System.out.println("I will execute at  first of test"); 
 	} 
 	@BeforeClass
 	public void beforeclass()
 	{
 		System.out.println("I will execute before methods of class day2"); 
+		System.out.println("I will execute before methods of class day2"); 
 	} 
 	@AfterClass
 	public void afterclass()
 	{
+		System.out.println("I will execute after methods of class day2"); 
 		System.out.println("I will execute after methods of class day2"); 
 	} 
 	
